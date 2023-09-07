02f38d9ddafc5822297967c0ae4a6a7ae26376d7
Idedav <xdavide.correa@gmail.com>
2023-09-07 20:21:03 +0200

    [FIX] message

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@
 const name = prompt('come ti chiami?')
 const lastname = prompt('qual è il tuo cognome?')
 const color = prompt('qual è il tuo colore preferito?')
+const password = name + lastname + color + '23'
 
 // MESSAGGIO DI OUTPUT
 const output = `
 <div class="container">
-    <h1>la tua password è ${name}${lastname}${color}23</h1>
+    <h1>la tua password è ${password}</h1>
 </div>
 `
 // COLLEGAMENTO DEL MESSAGGIO DI OUTPUT ALL ID
